1e8e711298fcfa2667c30710155afc5d1db36a10
mockingjay <8400684@qq.com>
2024-08-30 10:57:04 +0800

    ini

--- AvaloniaWin7Aot/AvaloniaWin7Aot/ViewModels/MainViewModel.cs
+++ AvaloniaWin7Aot/AvaloniaWin7Aot/ViewModels/MainViewModel.cs
@@ -4,6 +4,5 @@ namespace AvaloniaWin7Aot.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
 }
